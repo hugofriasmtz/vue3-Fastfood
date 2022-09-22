0eacfa02d1a4bf88a33e5344670e974c0ad05301
hugofriasmtz <a3521110393@alumno.uttehuacan.edu.mx>
2022-09-22 14:01:02 -0500

    modificacion de examen

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="col">
-        <div class="card">
+   <div class="col">
+        <div class="card text-center" style="width: 18rem;">
             <img :src="url" class="card-img-top" :alt="title">
             <div class="card-body">
                 <h5 class="text-center"> {{ title }} </h5>
@@ -9,7 +9,16 @@
                 <p class="text-center"> {{ price }} </p>
             </div>
         </div>
-    </div>
+   </div>
+   
+    
+
+    <!-- <div class="card" style="width: 18rem;">
+        <img src="..." class="card-img-top" alt="...">
+        <div class="card-body">
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+      </div> -->
 
 
 </template>   
