4a87f140f55c0b0fb20e58fa623bcfa1d4480339
hugofriasmtz <a3521110393@alumno.uttehuacan.edu.mx>
2022-09-22 17:04:24 -0500

    ultima Moodificacion

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,41 @@
 
 <template>
-  <header>
-    <div class="card text-center">
-        <div class="card-header border-dark">
-          <ul class="nav nav-pills card-header-pills">
-            <li class="nav-item">
-              <a class="nav-link active" href="#">Menú</a>
-            </li>
-          </ul>
-        </div>
-        <div class="card-body">
-            <h2 class="card-title">COMIDA RAPIDA</h2>
-
-          
-        </div>
+  
+  <nav class="navbar navbar-light bg-light">
+    <div class="container">
+      <a class="navbar-brand">
+        <img src="https://1000marcas.net/wp-content/uploads/2019/11/McDonalds-logo-768x432.png" height="50" alt="Logo" loading="lazy"/>
+      </a>
     </div>
-  </header>
+  </nav>
+
+  <br>
+
 
   <div class="row row-cols-1 row-cols-md-3 g-4">
-    <card url="https://cdn.noticiasdequeretaro.com.mx/wp-content/uploads/2022/01/27011056/20221251125115_1.jpg" title="Litter Cesar" name="Pizza Batman" description="pizza de pepperoni,con sabor a parmesano y mantequilla." price="$129 MXN"  /> 
-    <card url="https://cdn.computerhoy.com/sites/navi.axelspringer.es/public/styles/1200/public/media/image/2021/07/patatas-fritas-mcdonalds-2398633.jpg?itok=43Uuy6-m" title="Macdonald´s" name="Papas Fritas" description="Nuestras Papitas Mundialmente Famosas® de McDonald's ." price="$69.00 MXN." />
-    <card url="https://cdn.foodandwineespanol.com/2021/11/mcdonalds-mccrispy-1536x864.jpg" title="Macdonald´s" name="Hamburguesa" description="Viene cubierta con pepinillos, crujiente lechuga, cebollas y queso americano" price="$200 MXN." />
-    <card url="https://www.laespanolaaceites.com/wp-content/uploads/2019/05/espaguetis-a-la-bolonesa-1080x671.jpg" title="VIPS" name="Spaghetti" description=" Tiene forma de delgada cuerda larga" price="$70.00 MXN." />
-    <card url="https://cdn7.kiwilimon.com/recetaimagen/69/640x426/11549.jpg.webp" title="Carnitas Santa Elena" name="Empanadas" description="masa quebrada u hojaldre rellena con una preparación salada o dulce " price="$35 MXN." />
-    <card url="https://chefeel.com/chefgeneralfiles/2021/11/barbecue-grilled-hot-dogs-with-yellow-american-mustard-on-dark-wooden-background-880x541.jpg" title="Macdonald´s" name="Hot Dog" description="Panecillo largo lleno con una salchicha de Frankfurt" price="$55 MXN." />
+    
+    <card name="Pizza Batman" description="pizza de pepperoni,con sabor a parmesano y mantequilla." price="$129 MXN"  > <img src="https://cdn.noticiasdequeretaro.com.mx/wp-content/uploads/2022/01/27011056/20221251125115_1.jpg" alt=""></card>
+    <card name="Papas Fritas" description="Nuestras Papitas Mundialmente Famosas® de McDonald's ." price="$69.00 MXN." > <img src="https://cdn.computerhoy.com/sites/navi.axelspringer.es/public/styles/1200/public/media/image/2021/07/patatas-fritas-mcdonalds-2398633.jpg?itok=43Uuy6-m" alt=""> </card>
+    <card name="Hamburguesa" description="Viene cubierta con pepinillos, crujiente lechuga, cebollas y queso americano" price="$200 MXN." > <img src="https://cdn.foodandwineespanol.com/2021/11/mcdonalds-mccrispy-1536x864.jpg" alt=""> </card>
+    <card name="Spaghetti" description=" Tiene forma de delgada cuerda larga" price="$70.00 MXN." > <img src="https://www.laespanolaaceites.com/wp-content/uploads/2019/05/espaguetis-a-la-bolonesa-1080x671.jpg" alt=""> </card>
+    <card name="Empanadas" description="masa quebrada u hojaldre rellena con una preparación salada o dulce " price="$35 MXN." > <img src="https://cdn7.kiwilimon.com/recetaimagen/69/640x426/11549.jpg.webp" alt=""> </card>
+    <card name="Hot Dog" description="Panecillo largo lleno con una salchicha de Frankfurt" price="$55 MXN."> <img src="https://chefeel.com/chefgeneralfiles/2021/11/barbecue-grilled-hot-dogs-with-yellow-american-mustard-on-dark-wooden-background-880x541.jpg" alt=""></card>
   </div>
+  <br>
+  
+  <section class="">
+    <footer class="text-center text-white" style="background-color: #0a4275;"> 
+      <div class="container p-4 pb-0">
+        <section class="">
+          <p class="d-flex justify-content-center align-items-center">
+            <span class="me-3">Gracias</span>
+          </p>
+        </section>
+      </div>
+      <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);"> Hugo Frias Martinez</div>
+    </footer>
+  </section>
 
-  <footer>
-    <div class="card">
-        <div class="card-header border-dark">
-        </div>
-        <div class="card-body">
-        <h5 class="card-title">Gracias !</h5>
-        <p class="card-text">Vuelve Pronto :)</p>
-        </div>
-    </div>
-  </footer>
 </template>
 
 
--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,7 +1,7 @@
 <template>
-   <div class="col">
+    <div class="col">
         <div class="card text-center" style="width: 18rem;">
-            <img :src="url" class="card-img-top" :alt="title">
+            <slot></slot>  
             <div class="card-body">
                 <h5 class="text-center"> {{ title }} </h5>
                 <p class="text-center"> {{ name }}.</p>
@@ -9,29 +9,13 @@
                 <p class="text-center"> {{ price }} </p>
             </div>
         </div>
-   </div>
-   
-    
-
-    <!-- <div class="card" style="width: 18rem;">
-        <img src="..." class="card-img-top" alt="...">
-        <div class="card-body">
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        </div>
-      </div> -->
-
-
+    </div>
 </template>   
-
-
 
 <script>
 
     export default {
         props: {
-            url: {
-                String,
-            },
             title: {
                 String,
             },
@@ -49,3 +33,12 @@
 
     };
 </script>
+
+<style scoped>
+    img{
+        width: 18rem;
+        height: 9rem;
+        display: inline-block;
+    }
+    
+</style>
